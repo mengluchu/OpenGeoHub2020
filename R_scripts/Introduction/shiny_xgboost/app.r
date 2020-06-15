3b9df6cc22374f41e2364ae743389c1e7dd67bb9
Meng Lu <lumeng0312@gmail.com>
2020-06-16 00:29:59 +0200

    remove a few packages

--- R_scripts/Introduction/shiny_xgboost/app.r
+++ R_scripts/Introduction/shiny_xgboost/app.r
@@ -1,6 +1,6 @@
 library(shiny)
 library(devtools)
-install_github("mengluchu/APMtools")
+#install_github("mengluchu/APMtools")
 library(APMtools)
 library(ranger)
 library(raster)
@@ -8,7 +8,9 @@ library(dplyr)
 library(rasterVis)
 library(xgboost)
 library(RColorBrewer)
-
+library(data.table)
+library(Matrix)
+library(tibble)
 # process data 
 data("global_annual")
 y_var = "value_mean"
@@ -16,8 +18,8 @@ prestring =  "road|nightlight|population|temp|wind|trop|indu|elev"
 varstring = paste(prestring,y_var,sep="|")
 inde_var = global_annual%>%merge_roads( c(3, 4, 5), keep = F)%>%na.omit() %>%ungroup()%>%dplyr::select(matches(varstring))
 
-sr = stack("~/Documents/GitHub/OpenGeoHub2020/R_scripts/modeling_process/dc")
-
+sr = raster::stack("dc")
+ 
 ui =fluidPage(
   # *Input() functions,
   # *Output() function
@@ -26,7 +28,8 @@ ui =fluidPage(
   sliderInput(inputId='gamma', label= "penalty", min =1, max = 5, value = 1, step = 1),
   sliderInput(inputId='depth', label= "max. depth", min =1, max = 7, value = 3, step = 1),
   
-  textOutput(outputId = "bar"),
+  textOutput(outputId = "training"),
+  tableOutput(outputId = "test"),
   plotOutput(outputId = "image")
 )
 
@@ -45,15 +48,28 @@ server <- function(input, output){
                                   eta = input$eta, nthread = 4, gamma = input$gamma, nrounds = input$treenum, 
                                   verbose = 0)})
   
-  output$bar = renderPrint({
+  output$test = renderTable({
+     
+ crossvali =  function(n,df, y_var) {
+    smp_size <- floor(0.8 * nrow(df)) 
+    set.seed(n)
+    training<- sample(seq_len(nrow(df)), size = smp_size)
+    test = seq_len(nrow(df))[-training] 
+    P_xgb= xgboost_LUR(df, max_depth = input$depth, 
+                       eta = input$eta, nthread = 4, gamma = input$gamma, nrounds = input$treenum, y_varname= y_var,training=training, test=test, grepstring =varstring)
+     } 
+  V2 = lapply(1:5, df = merged, y_var = y_var,crossvali)
+  V2 = data.frame(five_fold_cv = rowMeans(data.frame(V2)))
+  rownames_to_column(V2, var = "accu. matrix")
+  })
+  output$training = renderPrint({
     print(data())
   })
   
   output$image <- renderPlot({   
     a= data()
     predfun <- function(model, data) {
-      v <- predict(model, as.matrix(data))
-    }
+    v <- predict(model, as.matrix(data)) }
     b= predict(sr, a, fun = predfun)
     myTheme = rasterTheme(region = rev(brewer.pal(7, "Spectral")))
     levelplot(b, par.settings = myTheme)
